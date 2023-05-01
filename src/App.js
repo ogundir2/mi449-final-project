54e0c780809ad98fc70b0a31c97125f7130a59fe
treasureogundiran <ogundirantreasure@gmail.com>
2023-05-01 19:40:27 -0400

    Added semester and course insertion

--- src/App.js
+++ src/App.js
@@ -11,6 +11,21 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import TextField from '@mui/material/TextField';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 
 const myTheme = createTheme({
   palette: {
@@ -52,6 +67,11 @@ function Content() {
   const [mySemesters, setMySemesters] = useState([]);
   const [myCourses, setMyCourses] = useState([]);
   const [clickedSemester, setClickedSemester] = useState(0);
+  const [season, setSeason] = useState('');
+  const [year, setYear] = useState(0);
+  const [courseName, setCourseName] = useState('');
+  const [credits, setCredits] = useState(0);
+  const [courseCompleted, setCourseCompleted] = useState(false);
   
 
   async function getSemesters() {
@@ -60,7 +80,6 @@ function Content() {
       .select("*");
     
     if (error) {
-      console.log(error);
     } else {
       setMySemesters(semesters);
     }
@@ -78,6 +97,7 @@ function Content() {
   }
 
   function handleClick(sem_id) {
+    console.log(sem_id);
     if (clickedSemester === sem_id) {
       setClickedSemester(0);
     } else {
@@ -85,19 +105,123 @@ function Content() {
     }
   }
 
+  const handleSeasonChange = (event) => {
+    setSeason(event.target.value);
+  };
+
+  const handleYearChange = (event, date) => {
+    console.log(event, date);
+    setYear(event.$y);
+  };
+
+  async function addSemester() {
+    if (!["Fall", "Spring", "Summer"].includes(season)) {
+      alert("Please select a valid season!");
+      return;
+    }
+
+    if (year < 1900 || year > 2100 ) {
+      alert("Please select a valid year!");
+      return;
+    }
+
+    const sem = `${season} ${year}`;
+
+    let duplicate = false;
+
+    mySemesters.forEach(mySem => {
+      if (mySem.semester_name === sem) {
+        duplicate = true;
+      }
+    });
+
+    if (duplicate) {
+      alert("The semester is already in your list!");
+      return;
+    }
+    
+    const { data, error } = await supabase
+    .from('semesters')
+    .insert([
+      { "semester_name": sem },
+    ])
+
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data)
+    }
+  }
+
+  const handleCourseNameChange = (event) => {
+    setCourseName(event.target.value);
+  };
+
+  const handleCreditsChange = (event) => {
+    setCredits(event.target.value);
+  };
+
+  const handleCourseCompletedChange = (event) => {
+    setCourseCompleted(event.target.checked);
+  };
+
+  async function addCourse() {
+    if (credits < 1 || credits > 4) {
+      alert("Please select a valid number of credits!");
+      return;
+    }
+
+    if (courseName.length < 2) {
+      alert("Please input a valid course name!");
+      return;
+    }
+
+    let duplicate = false;
+
+    myCourses.forEach(myCourse => {
+      if (myCourse.course_name.toLowerCase() === courseName.toLowerCase()) {
+        duplicate = true;
+      }
+    });
+
+    if (duplicate) {
+      alert("The course is already in your list!");
+      return;
+    }
+
+    if (clickedSemester <= 0) {
+      alert("Please select a semester before adding a course!");
+      return;
+    }
+    
+    console.log(courseName, credits, clickedSemester, courseCompleted);
+    
+    const { data, error } = await supabase
+    .from('courses')
+    .insert([
+      { "course_name": courseName, "semester_id": clickedSemester, "course_completed": courseCompleted, "course_credits": credits },
+    ])
+
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data)
+    }
+  }
+
   getSemesters();
   getCourses();
 
   return (
-    <Box sx={{ gridGap: 5, display: 'flex', padding: 5}}>
-      <Box sx={{ borderRadius: 2, padding: 1, flex: 1, backgroundColor: "special.main" }}>
+    <Box sx={{ gridGap: 10, display: 'flex', padding: 5, height: 0.75}}>
+      <Box sx={{ borderRadius: 2, padding: 2, flex: 1, backgroundColor: "special.main" }}>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Semesters
         </Typography>
-        <List>
+        <List sx={{height: 0.7, marginBottom: 1, overflowY: "auto"}}>
           {
             mySemesters.map(s => (
-              <ListItem >
+              <ListItem disablePadding sx={{ marginBottom: 1 }} >
                 {clickedSemester === s.id ? 
                 <ListItemButton sx={{ backgroundColor: "action.main", border: "solid", borderColor: "action.main", borderWidth: 1.5, borderRadius: 1, }} onClick={() => handleClick(s.id)}>
                   <ListItemText primary={s.semester_name} />
@@ -106,18 +230,47 @@ function Content() {
                   <ListItemText primary={s.semester_name} />
                 </ListItemButton>
               } 
-                
               </ListItem>
             ))
           }
         </List>
+        <Box>
+          <Typography sx={{marginBottom: 0.5, fontWeight: 500, fontSize: 14}} variant="p" component="p">
+            Add Semester
+          </Typography>
+          <Box sx={{ display: "flex", }}>
+            <FormControl sx={{ width: 1, marginRight: 1 }} size="medium">
+              <InputLabel>Season</InputLabel>
+              <Select
+                sx={{ borderColor: "primary.main" }}
+                value={season}
+                label="Season"
+                onChange={handleSeasonChange}
+              >
+                <MenuItem value="Fall">Fall</MenuItem>
+                <MenuItem value="Spring">Spring</MenuItem>
+                <MenuItem value="Summer">Summer</MenuItem>
+              </Select>
+            </FormControl>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker onChange={handleYearChange.bind(this)} sx={{ marginRight: 1}} label={'Year'} openTo="year" views={['year']} />
+            </LocalizationProvider>
+            <Button onClick={addSemester} variant="contained" aria-label="add-semester" size="small">
+              <AddIcon sx={{ color: "primary.contrast", fontSize: 28 }} />
+            </Button>
+          </Box>
+        </Box>
       </Box>
 
-      <Box sx={{borderRadius: 2, padding: 1, flex: 1, backgroundColor: "special.main"}}>
+      <Box sx={{borderRadius: 2, padding: 2, flex: 1, backgroundColor: "special.main"}}>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Courses
+          {
+            clickedSemester > 0 ?
+            `${mySemesters.filter(mySem => mySem.id === clickedSemester)[0].semester_name} Courses`:
+            'All Courses'
+          }
         </Typography>
-        <List>
+        <List sx={{height: 0.7, marginBottom: 1, overflowY: "auto"}}>
         {
             myCourses.filter(c => {
               if (clickedSemester === 0 ) return true;
@@ -126,14 +279,47 @@ function Content() {
 
               return false;
             }).map(c => (
-              <ListItem disablePadding>
-                
+              <ListItem>
+                <ListItemButton sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
                   <ListItemText primary={c.course_name} secondary={"Credits: "+c.course_credits}/>
-                
+                  {
+                    c.course_completed ? 
+                    <CheckCircleIcon sx={{color: "action.main"}} /> :
+                    <RadioButtonUncheckedIcon sx={{color: "action.main"}} />
+                  }
+                </ListItemButton>
               </ListItem>
             ))
           }
         </List>
+        <Box>
+          <Typography sx={{marginBottom: 0.5, fontWeight: 500, fontSize: 14}} variant="p" component="p">
+            Add Course
+          </Typography>
+          <Box sx={{ display: "flex", }}>
+          <TextField value={courseName} onChange={handleCourseNameChange} sx={{ width: 1, marginRight: 1 }} id="outlined-basic" label="Course Name" variant="outlined" />
+          <FormControl sx={{ width: 1, marginRight: 1 }} size="medium">
+              <InputLabel>Credits</InputLabel>
+              <Select
+                sx={{ borderColor: "primary.main" }}
+                value={credits}
+                label="Credits"
+                onChange={handleCreditsChange}
+              >
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+                <MenuItem value={4}>4</MenuItem>
+              </Select>
+            </FormControl>
+            <FormGroup>
+              <FormControlLabel onChange={handleCourseCompletedChange} labelPlacement="top" control={<Checkbox />} label="Completed" />
+            </FormGroup>
+          <Button onClick={addCourse} variant="contained" aria-label="add-course" size="small">
+            <AddIcon sx={{ color: "primary.contrast", fontSize: 28 }} />
+          </Button>
+        </Box>
+        </Box>
       </Box>
     </Box>
   );
@@ -142,7 +328,7 @@ function Content() {
 function App() {
   return (
     <ThemeProvider theme={myTheme}>
-      <div>
+      <div className="app-container">
         <ButtonAppBar />
         <Content/>
       </div>
