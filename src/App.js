c255da1429b4ad471d4a1993987dd940601f5575
treasureogundiran <ogundirantreasure@gmail.com>
2023-05-01 20:06:25 -0400

    Added progress bar

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import LinearProgress from '@mui/material/LinearProgress';
 
 const myTheme = createTheme({
   palette: {
@@ -72,6 +73,7 @@ function Content() {
   const [courseName, setCourseName] = useState('');
   const [credits, setCredits] = useState(0);
   const [courseCompleted, setCourseCompleted] = useState(false);
+  const [progress, setProgress] = useState(0);
   
 
   async function getSemesters() {
@@ -93,6 +95,17 @@ function Content() {
       if (error) {
       } else {
         setMyCourses(courses);
+        
+        let totalCrdts = 0;
+        let completedCrdts = 0;
+        for (let i=0; i < myCourses.length; i++) {
+          let c = myCourses[i];
+          totalCrdts += c.course_credits;
+
+          if (c.course_completed) completedCrdts += c.course_credits;
+        }
+
+        setProgress(completedCrdts * 100 / totalCrdts);
       }
   }
 
@@ -213,112 +226,129 @@ function Content() {
   getCourses();
 
   return (
-    <Box sx={{ gridGap: 10, display: 'flex', padding: 5, height: 0.75}}>
-      <Box sx={{ borderRadius: 2, padding: 2, flex: 1, backgroundColor: "special.main" }}>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Semesters
-        </Typography>
-        <List sx={{height: 0.7, marginBottom: 1, overflowY: "auto"}}>
-          {
-            mySemesters.map(s => (
-              <ListItem disablePadding sx={{ marginBottom: 1 }} >
-                {clickedSemester === s.id ? 
-                <ListItemButton sx={{ backgroundColor: "action.main", border: "solid", borderColor: "action.main", borderWidth: 1.5, borderRadius: 1, }} onClick={() => handleClick(s.id)}>
-                  <ListItemText primary={s.semester_name} />
-                </ListItemButton> : 
-                <ListItemButton sx={{ border: "solid", borderColor: "action.main", borderWidth: 1.5, borderRadius: 1, }} onClick={() => handleClick(s.id)}>
-                  <ListItemText primary={s.semester_name} />
-                </ListItemButton>
-              } 
-              </ListItem>
-            ))
-          }
-        </List>
-        <Box>
-          <Typography sx={{marginBottom: 0.5, fontWeight: 500, fontSize: 14}} variant="p" component="p">
-            Add Semester
-          </Typography>
-          <Box sx={{ display: "flex", }}>
-            <FormControl sx={{ width: 1, marginRight: 1 }} size="medium">
-              <InputLabel>Season</InputLabel>
-              <Select
-                sx={{ borderColor: "primary.main" }}
-                value={season}
-                label="Season"
-                onChange={handleSeasonChange}
-              >
-                <MenuItem value="Fall">Fall</MenuItem>
-                <MenuItem value="Spring">Spring</MenuItem>
-                <MenuItem value="Summer">Summer</MenuItem>
-              </Select>
-            </FormControl>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DatePicker onChange={handleYearChange.bind(this)} sx={{ marginRight: 1}} label={'Year'} openTo="year" views={['year']} />
-            </LocalizationProvider>
-            <Button onClick={addSemester} variant="contained" aria-label="add-semester" size="small">
-              <AddIcon sx={{ color: "primary.contrast", fontSize: 28 }} />
-            </Button>
+    <Box sx={{height: 0.75, padding: 5}}>
+
+      <Box sx={{marginBottom: 3}}>
+        Course Completion %
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box sx={{ width: '100%', mr: 1 }}>
+            <LinearProgress variant="determinate" value={progress} />
+          </Box>
+          <Box sx={{ minWidth: 35 }}>
+            <Typography variant="body2" color="text.secondary">{`${Math.round(
+              progress,
+            )}%`}</Typography>
           </Box>
         </Box>
       </Box>
-
-      <Box sx={{borderRadius: 2, padding: 2, flex: 1, backgroundColor: "special.main"}}>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          {
-            clickedSemester > 0 ?
-            `${mySemesters.filter(mySem => mySem.id === clickedSemester)[0].semester_name} Courses`:
-            'All Courses'
-          }
-        </Typography>
-        <List sx={{height: 0.7, marginBottom: 1, overflowY: "auto"}}>
-        {
-            myCourses.filter(c => {
-              if (clickedSemester === 0 ) return true;
-
-              if (c.semester_id === clickedSemester) return true;
-
-              return false;
-            }).map(c => (
-              <ListItem>
-                <ListItemButton sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-                  <ListItemText primary={c.course_name} secondary={"Credits: "+c.course_credits}/>
-                  {
-                    c.course_completed ? 
-                    <CheckCircleIcon sx={{color: "action.main"}} /> :
-                    <RadioButtonUncheckedIcon sx={{color: "action.main"}} />
-                  }
-                </ListItemButton>
-              </ListItem>
-            ))
-          }
-        </List>
-        <Box>
-          <Typography sx={{marginBottom: 0.5, fontWeight: 500, fontSize: 14}} variant="p" component="p">
-            Add Course
+    
+      <Box sx={{ gridGap: 10, display: 'flex', height: 0.9}}>
+        <Box sx={{ borderRadius: 2, padding: 2, flex: 1, backgroundColor: "special.main" }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Semesters
           </Typography>
-          <Box sx={{ display: "flex", }}>
-          <TextField value={courseName} onChange={handleCourseNameChange} sx={{ width: 1, marginRight: 1 }} id="outlined-basic" label="Course Name" variant="outlined" />
-          <FormControl sx={{ width: 1, marginRight: 1 }} size="medium">
-              <InputLabel>Credits</InputLabel>
-              <Select
-                sx={{ borderColor: "primary.main" }}
-                value={credits}
-                label="Credits"
-                onChange={handleCreditsChange}
-              >
-                <MenuItem value={1}>1</MenuItem>
-                <MenuItem value={2}>2</MenuItem>
-                <MenuItem value={3}>3</MenuItem>
-                <MenuItem value={4}>4</MenuItem>
-              </Select>
-            </FormControl>
-            <FormGroup>
-              <FormControlLabel onChange={handleCourseCompletedChange} labelPlacement="top" control={<Checkbox />} label="Completed" />
-            </FormGroup>
-          <Button onClick={addCourse} variant="contained" aria-label="add-course" size="small">
-            <AddIcon sx={{ color: "primary.contrast", fontSize: 28 }} />
-          </Button>
+          <List sx={{height: 0.7, marginBottom: 1, overflowY: "auto"}}>
+            {
+              mySemesters.map(s => (
+                <ListItem disablePadding sx={{ marginBottom: 1 }} >
+                  {clickedSemester === s.id ? 
+                  <ListItemButton sx={{ backgroundColor: "action.main", border: "solid", borderColor: "action.main", borderWidth: 1.5, borderRadius: 1, }} onClick={() => handleClick(s.id)}>
+                    <ListItemText primary={s.semester_name} />
+                  </ListItemButton> : 
+                  <ListItemButton sx={{ border: "solid", borderColor: "action.main", borderWidth: 1.5, borderRadius: 1, }} onClick={() => handleClick(s.id)}>
+                    <ListItemText primary={s.semester_name} />
+                  </ListItemButton>
+                } 
+                </ListItem>
+              ))
+            }
+          </List>
+          <Box>
+            <Typography sx={{marginBottom: 0.5, fontWeight: 500, fontSize: 14}} variant="p" component="p">
+              Add Semester
+            </Typography>
+            <Box sx={{ display: "flex", }}>
+              <FormControl sx={{ width: 1, marginRight: 1 }} size="medium">
+                <InputLabel>Season</InputLabel>
+                <Select
+                  sx={{ borderColor: "primary.main" }}
+                  value={season}
+                  label="Season"
+                  onChange={handleSeasonChange}
+                >
+                  <MenuItem value="Fall">Fall</MenuItem>
+                  <MenuItem value="Spring">Spring</MenuItem>
+                  <MenuItem value="Summer">Summer</MenuItem>
+                </Select>
+              </FormControl>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker onChange={handleYearChange.bind(this)} sx={{ marginRight: 1}} label={'Year'} openTo="year" views={['year']} />
+              </LocalizationProvider>
+              <Button onClick={addSemester} variant="contained" aria-label="add-semester" size="small">
+                <AddIcon sx={{ color: "primary.contrast", fontSize: 28 }} />
+              </Button>
+            </Box>
+          </Box>
         </Box>
+
+        <Box sx={{borderRadius: 2, padding: 2, flex: 1, backgroundColor: "special.main"}}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            {
+              clickedSemester > 0 ?
+              `${mySemesters.filter(mySem => mySem.id === clickedSemester)[0].semester_name} Courses`:
+              'All Courses'
+            }
+          </Typography>
+          <List sx={{height: 0.7, marginBottom: 1, overflowY: "auto"}}>
+          {
+              myCourses.filter(c => {
+                if (clickedSemester === 0 ) return true;
+
+                if (c.semester_id === clickedSemester) return true;
+
+                return false;
+              }).map(c => (
+                <ListItem disablePadding>
+                  <ListItemButton sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                    <ListItemText primary={c.course_name} secondary={"Credits: "+c.course_credits}/>
+                    {
+                      c.course_completed ? 
+                      <CheckCircleIcon sx={{color: "action.main"}} /> :
+                      <RadioButtonUncheckedIcon sx={{color: "action.main"}} />
+                    }
+                  </ListItemButton>
+                </ListItem>
+              ))
+            }
+          </List>
+          <Box>
+            <Typography sx={{marginBottom: 0.5, fontWeight: 500, fontSize: 14}} variant="p" component="p">
+              Add Course
+            </Typography>
+            <Box sx={{ display: "flex", }}>
+            <TextField value={courseName} onChange={handleCourseNameChange} sx={{ width: 1, marginRight: 1 }} id="outlined-basic" label="Course Name" variant="outlined" />
+            <FormControl sx={{ width: 1, marginRight: 1 }} size="medium">
+                <InputLabel>Credits</InputLabel>
+                <Select
+                  sx={{ borderColor: "primary.main" }}
+                  value={credits}
+                  label="Credits"
+                  onChange={handleCreditsChange}
+                >
+                  <MenuItem value={1}>1</MenuItem>
+                  <MenuItem value={2}>2</MenuItem>
+                  <MenuItem value={3}>3</MenuItem>
+                  <MenuItem value={4}>4</MenuItem>
+                </Select>
+              </FormControl>
+              <FormGroup>
+                <FormControlLabel onChange={handleCourseCompletedChange} labelPlacement="top" control={<Checkbox />} label="Completed" />
+              </FormGroup>
+            <Button onClick={addCourse} variant="contained" aria-label="add-course" size="small">
+              <AddIcon sx={{ color: "primary.contrast", fontSize: 28 }} />
+            </Button>
+          </Box>
+          </Box>
         </Box>
       </Box>
     </Box>
